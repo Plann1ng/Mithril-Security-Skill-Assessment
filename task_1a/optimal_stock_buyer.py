612e1ddc5efb73c0c0ed0364fd7d720bf3508e56
Ryustem Shaban <rs223fx@student.lnu.se>
2024-06-10 03:21:26 +0200

    Added feedback for incorrect list lenght

--- task_1a/optimal_stock_buyer.py
+++ task_1a/optimal_stock_buyer.py
@@ -1,17 +1,21 @@
 def when_to_buy(prices):
     # Ensuring that the list is in the proper range
     if not (2 <= len(prices) < 8):
-        print("This can't be used")
+        raise ValueError("Number of prices must be between 2 and 7 for this function to work properly.")
+
     # Resulting string will be stored here
     result = []
+
     # Lenght of the given list should be value (n)
     n = len(prices)
+
     # Start iteration from day 2
     for i in range(1, n):
         if prices[i] > prices[i - 1]:
             result.append((i - 1, i))
             print(f"Transaction found: Buy on day {i}, Sell on day {i + 1}")
-    return 
+    return prices
+
 prices = [45, 20, 30, 35, 32]
 transactions = when_to_buy(prices)
 
