444bd322961da2f1ef6710dcd2eff7e28ef9e6db
Ryustem Shaban <rs223fx@student.lnu.se>
2024-06-10 12:19:44 +0200

    Starting with task 2

---
+++ task_1b/stock_predictions.py
@@ -0,0 +1,7 @@
+def optimal_plan(low_risk, high_risk):
+    n = len(low_risk)
+    total = 0
+    return total
+low_risk = [10, 1, 10, 10]
+high_risk = [5, 50, 5, 1]
+print(optimal_plan(low_risk, high_risk))  # Expected output should be 70
