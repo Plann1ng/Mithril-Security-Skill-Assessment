ef9297ddde263387ff648a238ee2c130a6c254c7
Ryustem Shaban <rs223fx@student.lnu.se>
2024-06-10 12:50:37 +0200

    Final test has been added

--- task_1b/tests.py
+++ task_1b/tests.py
@@ -44,5 +44,23 @@ class TestOptimalPlan(unittest.TestCase):
         high_risk = [20]
         self.assertEqual(optimal_plan(low_risk, high_risk), 10)
         print("Single element test passed.")
+
+    def test_alternating_values(self):
+        low_risk = [5, 5, 5, 5, 5]
+        high_risk = [10, 1, 10, 1, 10]
+        self.assertEqual(optimal_plan(low_risk, high_risk), 25)
+        print("Alternating values test passed.")
+
+    def test_increasing_sequence(self):
+        low_risk = [1, 2, 3, 4, 5]
+        high_risk = [6, 7, 8, 9, 10]
+        self.assertEqual(optimal_plan(low_risk, high_risk), 10)
+        print("Increasing sequence test passed.")
+
+    def test_decreasing_sequence(self):
+        low_risk = [5, 4, 3, 2, 1]
+        high_risk = [10, 9, 8, 7, 6]
+        self.assertEqual(optimal_plan(low_risk, high_risk), 6)
+        print("Decreasing sequence test passed.")
 if __name__ == '__main__':
     unittest.main()
