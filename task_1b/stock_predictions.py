1fb5e5eb1235d747404760bb103c488159263ac6
Ryustem Shaban <rs223fx@student.lnu.se>
2024-06-10 12:31:51 +0200

    Logic implemented

--- task_1b/stock_predictions.py
+++ task_1b/stock_predictions.py
@@ -1,5 +1,5 @@
 def optimal_plan(low_risk, high_risk):
-# Lists must be the same lenght
+    # Lists must be the same lenght
     n = len(low_risk)
 
     # Optimal output
@@ -13,11 +13,23 @@ def optimal_plan(low_risk, high_risk):
     elif (len(low_risk) != len(high_risk)):
         raise ValueError("High-risk and Low-risk lenght must be the same!")
     
-    # Initialize the first week
+    # First week always low risk since there is no possible n-1
     last_added = low_risk[0] # Last_added index so we can keep track and remove later if the upcoming week high risk is rewarding
     total += last_added
 
+    # Starting from the second week
+    for i in range(1, n):
+    # If current week high risk is better than low risk, remove the previously added number from total
+        if high_risk[i] > low_risk[i]:
+            total -= last_added
+            last_added = high_risk[i]
+            total += last_added
+    # If low risk is better, then just add
+        else:
+            last_added = low_risk[i]
+            total += last_added
     return total
+
 low_risk = [10, 1, 10, 10]
 high_risk = [5, 50, 5, 1]
 print(optimal_plan(low_risk, high_risk))  # Expected output should be 70
