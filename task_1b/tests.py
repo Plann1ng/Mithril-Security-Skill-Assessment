ccb11cfc6bbdd759cc2a1e0ae78de175bd7c9fd8
Ryustem Shaban <rs223fx@student.lnu.se>
2024-06-10 12:43:51 +0200

    Test all low and test all high risk have been added

--- task_1b/tests.py
+++ task_1b/tests.py
@@ -14,6 +14,17 @@ class TestOptimalPlan(unittest.TestCase):
         high_risk = []
         self.assertEqual(optimal_plan(low_risk, high_risk), 0)
         print("No weeks test passed.")
-        
+
+    def test_all_low_risk(self):
+        low_risk = [10, 10, 10, 10]
+        high_risk = [5, 5, 5, 5]
+        self.assertEqual(optimal_plan(low_risk, high_risk), 40)
+        print("All low risk test passed.")
+
+    def test_all_high_risk(self):
+        low_risk = [1, 1, 1, 1]
+        high_risk = [10, 10, 10, 10]
+        self.assertEqual(optimal_plan(low_risk, high_risk), 10)  # High risk always chosen
+        print("All high risk test passed.")
 if __name__ == '__main__':
     unittest.main()
