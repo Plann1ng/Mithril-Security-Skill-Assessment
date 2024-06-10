0d6eae6dc49485ae6041ac8ad3f8a32309570094
Ryustem Shaban <rs223fx@student.lnu.se>
2024-06-10 03:18:28 +0200

    Algorithm logic finalised

--- task_1a/optimal_stock_buyer.py
+++ task_1a/optimal_stock_buyer.py
@@ -6,6 +6,11 @@ def when_to_buy(prices):
     result = []
     # Lenght of the given list should be value (n)
     n = len(prices)
+    # Start iteration from day 2
+    for i in range(1, n):
+        if prices[i] > prices[i - 1]:
+            result.append((i - 1, i))
+            print(f"Transaction found: Buy on day {i}, Sell on day {i + 1}")
     return 
 prices = [45, 20, 30, 35, 32]
 transactions = when_to_buy(prices)
