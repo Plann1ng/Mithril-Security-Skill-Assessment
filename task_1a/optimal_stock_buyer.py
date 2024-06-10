bb9b17bdf137e31c77c47d0519a444625904c574
Ryustem Shaban <rs223fx@student.lnu.se>
2024-06-10 03:13:45 +0200

    Check for list range added

--- task_1a/optimal_stock_buyer.py
+++ task_1a/optimal_stock_buyer.py
@@ -1,6 +1,12 @@
 def when_to_buy(prices):
-
-# Example usage:
+    # Ensuring that the list is in the proper range
+    if not (2 <= len(prices) < 8):
+        print("This can't be used")
+    # Resulting string will be stored here
+    result = []
+    # Lenght of the given list should be value (n)
+    n = len(prices)
+    return 
 prices = [45, 20, 30, 35, 32]
 transactions = when_to_buy(prices)
 
